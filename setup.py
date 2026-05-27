2a3bae3488768ff44928a1716b18834af18ac1aa
sansh0u <1952730137@qq.com>
2026-05-27 15:27:29 +0800

    1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Multi-omics Spatial Toolkit",
+    name="most",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[

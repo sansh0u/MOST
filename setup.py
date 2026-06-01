4102f7c6304f52c52648b532207329a24ab616ca
sansh0u <1952730137@qq.com>
2026-06-01 16:19:44 +0800

    6.1

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="most",
     version="0.1.0",
     packages=find_packages(),
-    package_dir= {},
     package_data={"most.barcode": ["*.txt"],"most.config": ["*.yaml"],},
     install_requires=[
         "stpipeline>=2.1.0" 

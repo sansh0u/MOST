4d84af3d197bd9d3a103310ee9e3f3ce96c92855
sansh0u <1952730137@qq.com>
2026-06-01 15:11:45 +0800

    6.1

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="most",
     version="0.1.0",
-    packages=find_packages(),
+    
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     install_requires=[
         "stpipeline>=2.1.0" 
     ],
@@ -13,10 +15,12 @@ setup(
         ],
     },
     include_package_data=True,
+    packages=find_packages(),
+    package_dir= {},
     package_data={
         "most": [
             "barcode/*.txt",
-            "config/*.yaml",
+            "config/*.yaml"
         ],
     }
 )

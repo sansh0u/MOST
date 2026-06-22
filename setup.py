ea40cb1893cd1a95a2cb036b1f6c8163fa3048a3
sansh0u <1952730137@qq.com>
2026-06-22 17:00:10 +0800

    6.22

--- setup.py
+++ setup.py
@@ -1,19 +1,18 @@
 from setuptools import setup, find_packages
+
 setup(
-    name="most", 
+    name="most",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     packages=find_packages(),
     include_package_data=True,
-    package_data={"most": ["barcode/*.txt","config/*.yaml",]},
-    install_requires=[
-        "stpipeline>=2.1.0", 
-        "plotly==5.24.1",
-        "kaleido==0.2.1"
-    ],
+    package_data={
+        "most": ["barcode/*.txt", "config/*.yaml"]
+    },
+    install_requires=[],
     entry_points={
         "console_scripts": [
-            "most = most.CLI:app",  #修改toolkit名字
+            "most = most.CLI:app",
         ],
     },
 )
